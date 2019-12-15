c7f06de2009cfd85434e3b54791f34da0fce00ee
lucaspdude <lucaspdude@gmail.com>
2019-12-15 00:31:56 -0300

    updating app with Shop Page and Collection Preview

--- src/App.js
+++ src/App.js
@@ -7,19 +7,15 @@ import HomePage from './pages/homepage/homepage.component';
 
 import './pages/homepage/homepage.styles.scss';
 
+import ShopPage from './pages/pages/shop.component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
 
 
 function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop/' component={ShopPage} />
     </div>
   );
 }
